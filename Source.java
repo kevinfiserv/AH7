ca55badb3a5dd970a623a898c880c61b11290f0c
Kevin Ramos <kevin.ramos@fiserv.com>
2023-11-02 13:06:25 -0400

    solution

--- Source.java
+++ Source.java
@@ -11,7 +11,13 @@ class Node<T> {
 class Source {
   public static int sumList(Node<Integer> head) {
     // todo
-    return;
+    int sum = 0;
+    while (head != null) {
+      sum += head.val;
+      head = head.next;
+
+    }
+    return sum;
   }
 
   public static void main(String[] args) {
@@ -27,8 +33,25 @@ class Source {
     d.next = e;
 
     // 2 -> 8 -> 3 -> -1 -> 7
-    
+
     System.out.println("Sum of my list: ");
     System.out.println(Source.sumList(a));
+
+    Node<Integer> r = new Node<>(1);
+    Node<Integer> aa= new Node<>(2);
+    Node<Integer> m = new Node<>(3);
+    Node<Integer> o = new Node<>(4);
+    Node<Integer> s = new Node<>(5);
+
+    r.next = aa;
+    aa.next = m;
+    m.next = o;
+    o.next = s;
+
+
+    // 2 -> 8 -> 3 -> -1 -> 7
+
+    System.out.println("Sum of my list: ");
+    System.out.println(Source.sumList(r));
   }
 }
